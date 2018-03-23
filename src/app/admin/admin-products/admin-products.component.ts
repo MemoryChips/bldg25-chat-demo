5de52dbc5752310691f4fa721c8d349478799862
Rob Tamlyn <MemoryChips@yahoo.com>
2018-03-23 19:18:31 -0400

    Adds new material table to admin products

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,94 +1,69 @@
-import { Component, OnInit, OnDestroy } from '@angular/core'
-// import { Router } from '@angular/router'
-import { DataTableConfig, DataTableService, Column } from 'ng5-data-table'
-
-import { Observable } from 'rxjs/Observable'
-import { Subscription } from 'rxjs/Subscription'
-
-import { Product, ProductService } from '../../shared/services/product.service'
-import { ActionComponent } from 'app/admin/admin-products/action/action.component'
-
+import { Component, ViewChild, OnInit, AfterViewInit } from '@angular/core'
+import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material'
+import { DomSanitizer } from '@angular/platform-browser'
+import { ProductService, Product } from '../../shared/services/product.service'
+/**
+ * @title Table with pagination
+ */
 @Component({
   selector: 'app-admin-products',
   templateUrl: './admin-products.component.html',
   styleUrls: ['./admin-products.component.scss']
 })
-export class AdminProductsComponent implements OnInit, OnDestroy {
+export class AdminProductsComponent implements OnInit, AfterViewInit {
+  // displayedColumns = ['key', 'category', 'title', 'price', 'imageUrl']
+  displayedColumns = ['title', 'action', 'category', 'price', 'image-url']
+  dataSource: MatTableDataSource<Product>
 
-  tableId: string
-  selectedRowIds: string[] = []
+  @ViewChild(MatPaginator) paginator: MatPaginator
+  @ViewChild(MatSort) sort: MatSort
 
-  private _subscriptions: Subscription[] = []
+  // private _subscriptions: Subscription[] = []
   constructor(
     private productService: ProductService,
-    private dataTableService: DataTableService,
-    // private router: Router
-  ) { }
+    private domSanitizer: DomSanitizer
+  ) {
+    this.dataSource = new MatTableDataSource<Product>([])
+  }
 
   ngOnInit() {
-    const data$: Observable<Product[]> = this.productService.getList()
-    this.dataTableService.createCellComponents([ActionComponent])
-    const columns: (Column | string)[] = [
-      'title',
-      {
-        key: 'price',
-        useCurrencyPipe: true,
-        width: '80px',
-        sortable: true,
-      },
-      {
-        key: 'category',
-        sortable: true,
-        width: '100px',
-      },
-      {
-        key: 'imageUrl',
-        title: 'Image',
-        width: '55px'
-      },
-      // example of a column with no data
-      {
-        key: 'action',
-        // component: 'ActionComponent',  // either way
-        // component: ActionComponent.name,  // maybe safer since no typos
-        component: ActionComponent,  // can also just pass the component type itself
-        title: 'Action',
-        width: '150px',
-      },
-    ]
-    const partialConfig: DataTableConfig = {
-      headerConfig: {
-        actionLinks: [
-          {
-            actionRoute: '/admin/products/new',
-            actionLinkText: 'New Service',
-            minSelections: 0,
-            maxSelections: 9999
-          }
-        ],
-      },
-      tableConfig: {
-        header: true,
-      }
-    }
-    this.tableId = this.dataTableService.createDataTableState(data$, columns, partialConfig)
-    this._subscriptions = [
-      this.dataTableService.getDataTableState(this.tableId).uiState$.subscribe(uiState => {
-        this.selectedRowIds = uiState.selectedRows
-      }),
-    ]
+    const data = this.productService.getListStatic()
+    this.productService.getList().subscribe(products => {
+      console.log(products)
+      // this.dataSource.data = products
+      this.dataSource.data = data
+    })
   }
 
-  ngOnDestroy() {
-    this._subscriptions.forEach(s => s.unsubscribe())
+  /**
+   * Set the paginator after the view init since this component will
+   * be able to query its view for the initialized paginator.
+   */
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator
+    this.dataSource.sort = this.sort
   }
 
-  // editProductClicked(product) {
-  //   this.router.navigate(['admin/products', product.key])
-  // }
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim() // Remove whitespace
+    filterValue = filterValue.toLowerCase() // Datasource defaults to lowercase matches
+    // this.dataSource.filter = ` ${filterValue}` // to force data source to filter
+    this.dataSource.filter = filterValue
+  }
 
-  // copyProductClicked(product: Product) {
-  //   this.router.navigate(['admin/products/copy', product.key])
-  // }
+  sanitize(imageUrl: string) {
+    return this.domSanitizer.bypassSecurityTrustStyle(`url(${imageUrl})`)
+  }
 
+  productEditClicked(item: Product) {
+    console.log(`Edit requested for ${item.title} with id ${item.key}`)
+  }
+
+  productCopyClicked(item) {
+    console.log(`Copy requested for ${item.title}`)
+  }
+
+  productDeleteClicked(item) {
+    console.log(`Delete requested for ${item.title}`)
+  }
 }

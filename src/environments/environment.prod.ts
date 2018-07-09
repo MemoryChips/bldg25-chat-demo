3ed7f4ae0fada0b2281b25cdfa1c6ff527aba517
Rob Tamlyn <MemoryChips@yahoo.com>
2018-07-09 15:21:43 -0400

    Updates build log messages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,18 @@
-const host = 'localhost'
-const port = 9000
+const host = 'localhost' // Set this to the host url of the deployment server
+const port = 4200 // Set this to the port of the deployment server
+const httpType = 'https'
+const webSocketProtocol = 'wss' // Keep this as wss unless you are deploying an unecrypted server
 
-// const host = process.env.HOST_URL || 'localhost'
-// const port = process.env.PORT || 4200
+const serverUrl = `${httpType}://${host}:${port}/api-ws`
+const webSocketServerUrl = `${webSocketProtocol}://${host}:${port}/api-ws`
+console.log(`Building for a server at ${serverUrl}`)
 
 export const environment = {
-  production: false,
-  webSocketServerUrl: `ws://${host}:${port}/api-ws`
+  production: true,
+  chatConfig: {
+    webSocketServerUrl,
+    chatOpenAtLogin: false,
+    reconnectInterval: 5000,
+    reconnectAttempts: 6
+  }
 }

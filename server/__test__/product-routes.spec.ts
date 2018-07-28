d361df6c869d1ec2b33eb332fa074ebca6ce398c
Rob Tamlyn <MemoryChips@yahoo.com>
2018-07-28 09:15:47 -0400

    Adds another product api test

--- server/__test__/product-routes.spec.ts
+++ server/__test__/product-routes.spec.ts
@@ -1,7 +1,8 @@
-// tslint:disable:max-line-length
 import { serverConfig } from './test-config'
 // import { testConfig } from './test-config'
 import fetch from 'node-fetch'
+
+import { DbProducts, DbProduct } from '../product/product-api'
 const verbose = false
 const serverType = 'http'
 
@@ -10,16 +11,33 @@ const serverUrl = `${serverType}://${serverConfig.chatServerHost}:${
 }`
 const productRequestUrl = `${serverUrl}/api/product`
 
+let fetchedProducts: DbProducts = {}
+let productKeys: string[] = []
+
 describe('Api product endpoints', () => {
   it(`should fetch product list`, (done: DoneFn) => {
     fetch(`${productRequestUrl}/all`)
       .then(res => res.json())
       .then(products => {
+        fetchedProducts = products
+        productKeys = Object.keys(fetchedProducts)
         if (verbose) {
           console.log(products)
         }
-        const numProducts = Object.keys(products).length
+        const numProducts = productKeys.length
         expect(numProducts).toBe(21)
+        done()
+      })
+  })
+  it(`should fetch the last product in the list`, (done: DoneFn) => {
+    const productId = productKeys[productKeys.length - 1]
+    fetch(`${productRequestUrl}/${productId}`)
+      .then(res => res.json())
+      .then((product: DbProduct) => {
+        if (verbose) {
+          console.log(product)
+        }
+        expect(product.title).toBe(fetchedProducts[productId].title)
         done()
       })
   })
